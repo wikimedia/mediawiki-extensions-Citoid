29458c1474b8b199d3833b2d6f5aea6a7db9d71b
Moriel Schottlender <moriel@gmail.com>
2015-03-31 11:32:24 -0700

    Insert inline transclusion instead of block
    The inserted citation should be an inline transclusion, not a block
    transclusion. This will also make sure the store preserves the
    rendering between citoid inspector and VE's reference context
    item.
    
    Change-Id: If6e29a769a11ac68fb622f14a70f2d669dc34e69

--- modules/ve.ui.CiteFromIdReferenceWidget.js
+++ modules/ve.ui.CiteFromIdReferenceWidget.js
@@ -47,12 +47,12 @@ ve.ui.CiteFromIdReferenceWidget = function VeUiCiteFromIdReferenceWidget( docume
 			documentModel.getStore(),
 			[
 				{
-					type: 'mwTransclusionBlock',
+					type: 'mwTransclusionInline',
 					attributes: {
 						mw: this.transclusionModel.getPlainObject()
 					}
 				},
-				{ type: '/mwTransclusionBlock' },
+				{ type: '/mwTransclusionInline' },
 				{ type: 'internalList' },
 				{ type: '/internalList' }
 			]
