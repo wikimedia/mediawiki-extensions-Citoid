a63cecc86d44f8e4ff772674a3186b6631dcc3b6
James D. Forrester <jforrester@wikimedia.org>
2015-05-08 10:45:08 -0700

    Follow-up 6a12b782: Remove VisualEditor's cite drop-down too
    Change-Id: Icce345f08cbee51404f9737787eddef39cacce94

--- modules/ve.ui.CiteFromIdInspectorTool.js
+++ modules/ve.ui.CiteFromIdInspectorTool.js
@@ -8,7 +8,7 @@
 
 	// HACK: Find the position of the current citation toolbar definition
 	// and manipulate it.
-	var i, len, originalDefinition,
+	var i, len,
 		toolGroups = ve.init.mw.Target.static.toolbarGroups,
 		citeIndex = toolGroups.length;
 
@@ -20,41 +20,12 @@
 	for ( i = 0, len = toolGroups.length; i < len; i++ ) {
 		if ( ve.getProp( toolGroups[i], 'include', 0, 'group' ) === 'cite' ) {
 			citeIndex = i;
-			originalDefinition = ve.copy( toolGroups[i] );
-			// Remove the label, since we only need a downward-arrow indicator
-			delete originalDefinition.label;
 			break;
 		}
 	}
 
-	// HACK: Replace the previous cite group to represent citoid and the
-	// dropdown menu for cite tools.
-	ve.init.mw.Target.static.toolbarGroups[ citeIndex ] = {
-		header: OO.ui.deferMsg( 'visualeditor-toolbar-cite-group' ),
-		title: OO.ui.deferMsg( 'visualeditor-toolbar-cite-group-tooltip' ),
-		include: [ 'citefromid', 'moreCiteTool' ]
-	};
-
-	/**
-	 * MediaWiki UserInterface citation tool group
-	 *
-	 * @class
-	 * @extends OO.ui.ToolGroupTool
-	 * @constructor
-	 * @param {OO.ui.ToolGroup} toolGroup
-	 * @param {Object} [config] Configuration options
-	 */
-	ve.ui.MoreCiteTool = function VeUiMoreCiteTool( toolGroup, config ) {
-		OO.ui.ToolGroupTool.call( this, toolGroup, config );
-	};
-	OO.inheritClass( ve.ui.MoreCiteTool, OO.ui.ToolGroupTool );
-	ve.ui.MoreCiteTool.static.autoAddToCatchall = false;
-	ve.ui.MoreCiteTool.static.name = 'moreCiteTool';
-	ve.ui.MoreCiteTool.static.group = 'citeTools';
-	ve.ui.MoreCiteTool.static.title =
-		OO.ui.deferMsg( 'visualeditor-toolbar-cite-tooltip' );
-	ve.ui.MoreCiteTool.static.groupConfig = originalDefinition;
-	ve.ui.toolFactory.register( ve.ui.MoreCiteTool );
+	// HACK: Replace the previous cite group with the citoid tool.
+	ve.init.mw.Target.static.toolbarGroups[ citeIndex ] = { include: [ 'citefromid' ] };
 
 	/**
 	 * MediaWiki UserInterface cite from ID inspector tool.
