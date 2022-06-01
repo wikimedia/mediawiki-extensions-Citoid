4835f8999e8fdf2e9ac1422e61fafc619ebfa70d
Ed Sanders <esanders@wikimedia.org>
2022-06-01 16:55:57 +0100

    VE: Make CitoidReferenceContextItem a real class
    This is possible now that we can manually suppress the
    generic reference context by name (done upstream).
    
    Bug: T310290
    Depends-On: Idbd475a68efd1aff14217de3a05fa3dedc744487
    Change-Id: I427df9a59eac9617e08a743073176bde63528781

---
+++ modules/ve/ve.ui.CitoidReferenceContextItem.js
@@ -0,0 +1,79 @@
+ve.ui.CitoidReferenceContextItem = function VeUiCitoidReferenceContextItem() {
+	// Parent constructor
+	ve.ui.CitoidReferenceContextItem.super.apply( this, arguments );
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ui.CitoidReferenceContextItem, ve.ui.MWReferenceContextItem );
+
+/**
+ * Get the href associated with this reference if it is a plain link reference
+ *
+ * @param {ve.dm.InternalItemNode} itemNode Reference item node
+ * @return {string|null} Href, or null if this isn't a plain link reference
+ */
+ve.ui.CitoidReferenceContextItem.static.getConvertibleHref = function ( itemNode ) {
+	var doc = itemNode.getRoot().getDocument(),
+		range = itemNode.getRange(),
+		// Get covering annotations
+		annotations = doc.data.getAnnotationsFromRange( range, false );
+
+	var annotation;
+	// The reference consists of one single external link so
+	// offer the user a conversion to citoid-generated reference
+	if (
+		annotations.getLength() === 1 &&
+		( annotation = annotations.get( 0 ) ) instanceof ve.dm.MWExternalLinkAnnotation
+	) {
+		return annotation.getHref();
+	} else if ( range.getLength() === 4 ) {
+		var contentNode = ve.getProp( itemNode, 'children', 0, 'children', 0 );
+		if ( contentNode instanceof ve.dm.MWNumberedExternalLinkNode ) {
+			return contentNode.getHref();
+		}
+	}
+	return null;
+};
+
+/**
+ * @inheritdoc
+ */
+ve.ui.CitoidReferenceContextItem.prototype.renderBody = function () {
+	var contextItem = this,
+		refNode = this.getReferenceNode();
+
+	// Parent method
+	ve.ui.CitoidReferenceContextItem.super.prototype.renderBody.call( this );
+
+	if ( !refNode || this.isReadOnly() || !ve.ui.mwCitoidMap ) {
+		return;
+	}
+
+	var convertibleHref = this.constructor.static.getConvertibleHref( refNode );
+
+	if ( convertibleHref ) {
+		var convertButton = new OO.ui.ButtonWidget( {
+			label: ve.msg( 'citoid-referencecontextitem-convert-button' )
+		} ).on( 'click', function () {
+			var action = ve.ui.actionFactory.create( 'citoid', contextItem.context.getSurface() );
+			action.open( true, convertibleHref );
+		} );
+
+		this.$body.append(
+			$( '<div>' )
+				.addClass( 've-ui-citoidReferenceContextItem-convert ve-ui-mwReferenceContextItem-muted' )
+				.text( ve.msg( 'citoid-referencecontextitem-convert-message' ) )
+		);
+
+		if ( this.$foot ) {
+			this.$foot.prepend( convertButton.$element );
+		} else {
+			this.$body.append( convertButton.$element );
+		}
+	}
+};
+
+/* Registration */
+
+ve.ui.contextItemFactory.register( ve.ui.CitoidReferenceContextItem );
