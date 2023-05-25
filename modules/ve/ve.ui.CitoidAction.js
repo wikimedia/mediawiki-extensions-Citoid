8f90680d7415c82737cc1fac4e3fa06e6b436cbb
Ed Sanders <esanders@wikimedia.org>
2023-05-25 16:26:38 +0100

    vu.ui.Citoid: Add missing arg to constructor documentation
    Change-Id: Ib921db1f4854084b8e3789c1bcac0bbd04c9e9ce

--- modules/ve/ve.ui.CitoidAction.js
+++ modules/ve/ve.ui.CitoidAction.js
@@ -14,6 +14,7 @@
  * @extends ve.ui.Action
  * @constructor
  * @param {ve.ui.Surface} surface Surface to act on
+ * @param {string} [source]
  */
 ve.ui.CitoidAction = function VeUiCitoidAction() {
 	// Parent constructor
