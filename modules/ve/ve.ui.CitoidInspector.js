53de1fe66983e450365dc45d8fae9f3fa56e4eaa
mareikeheuer <mareike.heuer@wikimedia.de>
2024-05-08 13:41:06 +0200

    Track Citoid service lookup failures
    Implemented general automatic reference fail tracker,
    as well as specific trackers for network errors and search result errors
    
    Bug: T363292
    Change-Id: I59069ed3e7b2ccd4b45dc6ed84d6bf453b0bc18d

--- modules/ve/ve.ui.CitoidInspector.js
+++ modules/ve/ve.ui.CitoidInspector.js
@@ -748,6 +748,9 @@ ve.ui.CitoidInspector.prototype.performLookup = function () {
 						.then( function () {
 							inspector.setModePanel( 'auto', 'result', false, { hasError: hasError } );
 						}, function () {
+							// Phabricator T363292
+							ve.track( 'activity.CitoidInspector', { action: 'automatic-generate-fail-searchResults' } );
+
 							inspector.lookupFailed();
 							return $.Deferred().resolve();
 						} );
@@ -758,6 +761,9 @@ ve.ui.CitoidInspector.prototype.performLookup = function () {
 				if ( response && response.textStatus === 'abort' ) {
 					return $.Deferred().reject();
 				}
+				// Phabricator T363292
+				ve.track( 'activity.CitoidInspector', { action: 'automatic-generate-fail-network' } );
+
 				inspector.lookupFailed();
 				// Restore focus to the input field.
 				// Definitely don't do this on success and focusing a hidden input causes jQuery
@@ -794,6 +800,9 @@ ve.ui.CitoidInspector.prototype.lookupFailed = function () {
 		this.updateSize();
 	}.bind( this ) ).setValidityFlag( false );
 	this.updateSize();
+
+	// Phabricator T363292
+	ve.track( 'activity.' + this.constructor.static.name, { action: 'automatic-generate-fail' } );
 };
 
 /**
